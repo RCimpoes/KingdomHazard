d1aecce36449e1cc5bd026bfd1b865b3d0da63a2
RCimpoesu <razvan.cimpoesu96@gmail.com>
2018-03-08 13:13:33 +0200

    First version of the app , with the following working feautures: -Functional menu to access the game -A version of the game that recognize multiple image targets and generates corespondent objects in AR

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class MainMenu : MonoBehaviour {
+
+        public void PlayGame(int indexScene)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("PAaa!");
+            Application.Quit();
+        }
+
+    }
+}
